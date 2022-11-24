4563402c4a1650b6acfa242efba662fa27e3811e
Setsuna Yukiii <siang-meng@live.com>
2022-11-24 15:44:45 +0800

    adding of pages

---
+++ src/Pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ErrorPage(props) {
+    return (
+        <div>
+            <h1>Sorry, nothing to look here.</h1>
+        </div>
+    );
+}
+
+export default ErrorPage;
