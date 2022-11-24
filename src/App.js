4e50c2e9e3e77d40519cba7a80975eabfab0062d
Setsuna Yukiii <siang-meng@live.com>
2022-11-24 16:13:47 +0800

    navbar & tailwind

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./App.css";
 import MainPage from "./Pages/MainPage";
 import ProjectPage from "./Pages/ProjectPage";
 import ErrorPage from "./Pages/ErrorPage";
+import Navbar from "./Components/Navbar";
 function App() {
     return (
         <div className="App">
+            <Navbar/>
             <BrowserRouter>
                 <Routes>
                     <Route exact path="/" element={<MainPage />}></Route>
--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function MainPage(props) {
     return (
         <div>
+            {/* <h1 className="text-3xls underline">Main Page</h1> */}
             <h1>Main Page</h1>
         </div>
     );
