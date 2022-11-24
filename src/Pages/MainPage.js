53b2b84dfb8395471e01462368c36556055f0157
Setsuna Yukiii <siang-meng@live.com>
2022-11-24 17:12:14 +0800

    getting skills, projects, experiences

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,9 +1,47 @@
-import React from "react";
-
-function MainPage(props) {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+function MainPage() {
+    const [skills, setSkills] = useState([]);
+    const [projects, setProjects] = useState([]);
+    const [experiences, setExperiences] = useState([]);
+    // get the data
+    const getSklls = async () => {
+        await axios
+            .get(process.env.REACT_APP_BACKEND_API + "/skills")
+            .then((res) => {
+                if(res.data.success){
+                    console.log("skills",res.data.data)
+                    setSkills(res.data.data)
+                }
+            });
+    };
+    const getProjects = async () => {
+        await axios
+            .get(process.env.REACT_APP_BACKEND_API + "/projects")
+            .then((res) => {
+                if(res.data.success){
+                    console.log("projects",res.data.data)
+                    setProjects(res.data.data)
+                }
+            });
+    };
+    const getExperiences = async () => {
+        await axios
+            .get(process.env.REACT_APP_BACKEND_API + "/experiences")
+            .then((res) => {
+                if(res.data.success){
+                    console.log("experience",res.data.data)
+                    setExperiences(res.data.data)
+                }
+            });
+    };
+    useEffect(() => {
+        getSklls();
+        getProjects();
+        getExperiences();
+    }, []);
     return (
         <div>
-            {/* <h1 className="text-3xls underline">Main Page</h1> */}
             <div>
                 <div className="card containers">
                     <h2>About me</h2>
