2ef4ceee3ca02db2fcb60b7427f820b959f78fba
Setsuna Yukiii <siang-meng@live.com>
2022-11-24 16:51:33 +0800

    the homepage layout

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -4,7 +4,20 @@ function MainPage(props) {
     return (
         <div>
             {/* <h1 className="text-3xls underline">Main Page</h1> */}
-            <h1>Main Page</h1>
+            <div>
+                <div className="card containers">
+                    <h2>About me</h2>
+                </div>
+                <div className="card containers">
+                    <h2>Skills</h2>
+                </div>
+                <div className="card containers">
+                    <h2>Projects</h2>
+                </div>
+                <div className="card containers">
+                    <h2>Experiences</h2>
+                </div>
+            </div>
         </div>
     );
 }
